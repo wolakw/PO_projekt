f6a7f1546265bba05f64d994e06abcbae76c421c
wolakw <wojtek.2017w@gmail.com>
2022-06-10 15:56:57 +0200

    przesylka dodaj

---
+++ src/Przesylka.java
@@ -0,0 +1,55 @@
+public class Przesylka implements Gabaryt{
+    private double dlugosc;
+    private double szerokosc;
+    private double wysokosc;
+    private double waga;
+    private String zawartosc;
+    private Klient nadawca;
+    private Klient odbiorca;
+
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Przesylka(double dlugosc, double szerokosc, double wysokosc, double waga, String zawartosc, Klient nadawca,
+                     Klient odbiorca){
+        this.dlugosc = dlugosc;
+        this.szerokosc = szerokosc;
+        this.wysokosc = wysokosc;
+        this.waga = waga;
+        this.zawartosc = zawartosc;
+        this.nadawca = nadawca;
+        this.odbiorca = odbiorca;
+    }
+
+    @Override
+    public String znajdzGabaryt() {
+        double v = dlugosc*szerokosc*wysokosc;
+        double A = 19456;
+        double B = 46208;
+        double C = 99712;
+
+        if (this.waga > 50){
+            return "niestandardowa";
+        }
+        else if (v < A){
+            return "A";
+        }
+        else if (v < B){
+            return "B";
+        }
+        else if (v < C){
+            return "C";
+        }
+        else{
+            return "niestandardowa";
+        }
+
+    }
+}
